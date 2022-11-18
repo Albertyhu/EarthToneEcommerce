5bfa5fc101f7b1f3900117010531c3d60668c720
Albert Hu <hualbert@rocketmail.com>
2022-11-17 20:23:08 -0800

    Fixed responsive issue on homepage

--- src/components/teaImages.js
+++ src/components/teaImages.js
@@ -1,5 +1,5 @@
 import BlackTea from '../images/tea/black_tea.jpg';
-import Gingseng from '../images/tea/ginseng_tea.jpg'; 
+import Gingseng from '../images/tea/GinsengProduct.jpg'; 
 import GreenTea from '../images/tea/green_tea.jpg';
 import IronBuddha from '../images/tea/Iron_buddha_tea.jpg'; 
 import JasmineTea from '../images/tea/jasmine_tea.jpg';
